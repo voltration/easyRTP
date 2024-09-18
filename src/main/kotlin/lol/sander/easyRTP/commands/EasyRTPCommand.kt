95934cb1d4b034895f8a9ddd6cc3d6c0c3d8ccbd
Sander <69873514+voltration@users.noreply.github.com>
2024-09-18 20:00:09 +0200

    2.0.0

--- src/main/kotlin/lol/sander/easyRTP/commands/EasyRTPCommand.kt
+++ src/main/kotlin/lol/sander/easyRTP/commands/EasyRTPCommand.kt
@@ -8,29 +8,41 @@ import org.bukkit.command.CommandSender
 
 class EasyRTPCommand : CommandExecutor {
 
+    private val spawnPlatformDecayEnabled = plugin.config.getBoolean("spawnPlatformDecayEnabled")
+    private val spawnPlatformDecayTime = plugin.config.getInt("spawnPlatformDecayTime")
     private val dontSpawnInWater = plugin.config.getBoolean("dontSpawnInWater")
     private val getHighestBlock = plugin.config.getBoolean("getHighestBlock")
     private val spawnPlatform = plugin.config.getBoolean("spawnPlatform")
     private val spawnPlatformSize = plugin.config.getInt("spawnPlatformSize")
-    private val tpdelay = plugin.config.getInt("tpDelay")
+    private val enablePrefix = plugin.config.getBoolean("enablePrefix")
+    private val prefix = plugin.config.getString("prefix")
+    private val tpDelay = plugin.config.getInt("tpDelay")
     private val min = plugin.config.getInt("minRange")
     private val max = plugin.config.getInt("maxRange")
 
+    private fun f(arg: String): String {
+        return formatMessage(arg, true)
+    }
+
     override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<out String>): Boolean {
-        sender.sendMessage(formatMessage("&d&lEasyRTP &7&o( https://github.com/voltration/easyRTP )"))
-        sender.sendMessage(formatMessage("&f"))
-        sender.sendMessage(formatMessage("&fdontSpawnInWater: ${formatAnswer(dontSpawnInWater)}"))
-        sender.sendMessage(formatMessage("&fgetHighestBlock: ${formatAnswer(getHighestBlock)}"))
-        sender.sendMessage(formatMessage("&fspawnPlatform: ${formatAnswer(spawnPlatform)}"))
-        sender.sendMessage(formatMessage("&fspawnPlatformSize: &e${spawnPlatformSize}x${spawnPlatformSize}"))
-        sender.sendMessage(formatMessage("&ftpDelay: &e${tpdelay}"))
-        sender.sendMessage(formatMessage("&fmaxRange: &e${max}"))
-        sender.sendMessage(formatMessage("&fminRange: &e${min}"))
+        sender.sendMessage(f("&d&lEasyRTP &7&o( https://github.com/voltration/easyRTP )"))
+        sender.sendMessage(f("&f"))
+        sender.sendMessage(f("&fspawnPlatformSize: &e${spawnPlatformSize}x${spawnPlatformSize}"))
+        sender.sendMessage(f("&fspawnPlatformDecayEnabled: ${formatAnswer(spawnPlatformDecayEnabled)}"))
+        sender.sendMessage(f("&fdontSpawnInWater: ${formatAnswer(dontSpawnInWater)}"))
+        sender.sendMessage(f("&fgetHighestBlock: ${formatAnswer(getHighestBlock)}"))
+        sender.sendMessage(f("&fspawnPlatform: ${formatAnswer(spawnPlatform)}"))
+        sender.sendMessage(f("&fenablePrefix: ${formatAnswer(enablePrefix)}"))
+        sender.sendMessage(f("&fspawnPlatformDecayTime: &e$spawnPlatformDecayTime"))
+        sender.sendMessage(f("&fprefix: $prefix"))
+        sender.sendMessage(f("&ftpDelay: &e${tpDelay}s"))
+        sender.sendMessage(f("&fmaxRange: &e$max"))
+        sender.sendMessage(f("&fminRange: &e$min"))
 
         return true
     }
 
     private fun formatAnswer(flag: Boolean): String {
-        return if (flag) "&a[ True ]" else "&c[ False ]"
+        return if (flag) "&a[True]" else "&c[False]"
     }
 }
--- src/main/kotlin/lol/sander/easyRTP/commands/RTP.kt
+++ src/main/kotlin/lol/sander/easyRTP/commands/RTP.kt
@@ -3,26 +3,28 @@ package lol.sander.easyRTP.commands
 import lol.sander.easyRTP.plugin
 import lol.sander.easyRTP.util.*
 import org.bukkit.Location
+import org.bukkit.Sound
 import org.bukkit.World
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import java.util.*
+import kotlin.collections.HashMap
 
 class RTP : CommandExecutor {
-
+    private val cooldowns: HashMap<UUID, Long> = HashMap()
+    private val cooldown = plugin.config.getInt("cooldown")
     private val dontSpawnInWater = plugin.config.getBoolean("dontSpawnInWater")
     private val getHighestBlock = plugin.config.getBoolean("getHighestBlock")
     private val spawnPlatform = plugin.config.getBoolean("spawnPlatform")
+    private val playSoundOnCountdown = plugin.config.getBoolean("playSoundOnCountdown")
+    private val sound = Sound.valueOf(plugin.config.getString("countdownSound") ?: "BLOCK_NOTE_BLOCK_PLING")
     private val delay = plugin.config.getInt("tpDelay")
 
     private fun teleportPlayerAsync(p: Player) {
-        if (dontSpawnInWater) {
-            teleportPlayerInSuitableLocationAsync(p, spawnPlatform)
-        }
-        else {
-            teleportPlayerInRandomLocationAsync(p, spawnPlatform)
-        }
+        if (dontSpawnInWater) teleportPlayerInSuitableLocationAsync(p, spawnPlatform)
+        else teleportPlayerInRandomLocationAsync(p, spawnPlatform)
     }
 
     private fun teleportPlayerInSuitableLocationAsync(p: Player, platform: Boolean) {
@@ -38,13 +40,16 @@ class RTP : CommandExecutor {
                 val y = if (getHighestBlock) p.world.getHighestBlockYAt(x, z) else generateRandomHeightCoordinate()
 
                 validLocation = checkSpawnArea(p, x, y, z)
-                location = Location(p.world, x.toDouble(), y.toDouble(), z.toDouble())
+
+                if (validLocation) {
+                    location = Location(p.world, x.toDouble() + 0.5, y.toDouble() + 1, z.toDouble() + 0.5)
+                }
             }
 
             // teleportation has to happen on the main thread
             plugin.server.scheduler.runTaskLater(plugin, Runnable {
                 location?.let {
-                    generateSpawnPlatform(p, it.blockY)
+                    if (platform) generateSpawnPlatform(p, it.blockX, it.blockY, it.blockZ)
                     p.teleport(it)
                 }
             }, delay * 20L)
@@ -59,18 +64,32 @@ class RTP : CommandExecutor {
         plugin.server.scheduler.runTaskAsynchronously(plugin, Runnable {
             val (x, z) = generateRandomCoordinates()
             val y = if (getHighestBlock) p.world.getHighestBlockYAt(x, z) else generateRandomHeightCoordinate()
-            val location = Location(p.world, x.toDouble(), y.toDouble(), z.toDouble())
+            val location = Location(p.world, x.toDouble() + 0.5, y.toDouble() + 1, z.toDouble() + 0.5)
 
             // teleportation has to happen on the main thread
             plugin.server.scheduler.runTaskLater(plugin, Runnable {
-                generateSpawnPlatform(p, location.blockY)
+                if (platform) generateSpawnPlatform(p, x, y, z)
                 p.teleport(location)
             }, delay * 20L)
         })
     }
 
     private fun sendMessage(p: Player) {
-        p.sendMessage(getMessageString("teleportingMessage").replace("{seconds}", delay.toString()))
+        var countdown = delay
+        plugin.server.scheduler.runTaskTimer(plugin, Runnable {
+            if (countdown > 0) {
+                if (playSoundOnCountdown) p.playSound(p.location, sound, 1.0F, 1.0F)
+                p.sendMessage(
+                    getMessageString("teleportingMessage").replace("{seconds}", countdown.toString()))
+                    countdown--
+            }
+
+            plugin.server.scheduler.cancelTask(this.hashCode())
+        }, 0L, 20L)
+    }
+
+    private fun setCooldown(p: Player) {
+        cooldowns[p.uniqueId] = System.currentTimeMillis() + (cooldown * 1000)
     }
 
     override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<out String>): Boolean {
@@ -81,6 +100,8 @@ class RTP : CommandExecutor {
         }
 
         val p: Player = sender
+        val cooldownEnd = cooldowns[p.uniqueId]
+        val currentTime = System.currentTimeMillis()
 
         if (!p.hasPermission("easyrtp.rtp")) {
             p.sendMessage(getMessageString("noPermissionMessage"))
@@ -92,6 +113,13 @@ class RTP : CommandExecutor {
             return true
         }
 
+        if (cooldownEnd != null && currentTime < cooldownEnd) {
+            val remainingTime = (cooldownEnd - currentTime) / 1000
+            p.sendMessage(getMessageString("cooldownMessage").replace("{seconds}", remainingTime.toString()))
+            return true
+        }
+
+        setCooldown(p)
         teleportPlayerAsync(p)
         return true
     }
